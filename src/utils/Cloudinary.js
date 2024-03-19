3b25c034e3b519d66a7d316f88482b0ffacb0e21
Abhi75033 <abhishekyadav1112.21@gmail.com>
2024-03-19 19:45:16 +0530

    Cloudinary Final config and the project setup has been completed

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -1,9 +1,26 @@
 import {v2 as cloudinary} from "cloudinary"
-
-          
+import fs from 'fs'
+ 
+// Cloudiary configuration 
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+
+const uploadOnCloudinary = async (localpath)=>{
+try {
+    if(!localpath) return null
+    const response = await  cloudinary.uploader.upload(localpath,{
+        resource_type:"auto",
+    })
+    // This is the above process for uploading the files on clouinary
+    console.log(`The file has been uplaoded successfully ${response.url}`);
+    return response
+} catch (error) {
+    fs.unlink(localpath)// file has been unllinked or deleted from our server if the the upoading
+    // failed
+    return null
+}
+}
